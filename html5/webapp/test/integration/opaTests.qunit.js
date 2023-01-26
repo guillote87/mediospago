a0da55b621fade4915ffa894c5abddfe8740aac8
Guillermo Quattrocchi <gq4dev@gmail.com>
2023-01-26 14:09:47 +0000

    First commit mediodepago

---
+++ html5/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["legacy/mediopago/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ html5/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"legacy/mediopago/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
